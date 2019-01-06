68027e5bf6a0f0284763460b7e8d6231ab9e3fc7
gilip <filipposa@csd.auth.gr>
2019-01-06 19:38:33 +0200

    partA

--- partA.py
+++ partA.py
@@ -3,9 +3,22 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import seaborn
+import pylab
 
+
+#first five elements of the csv
 pd.set_option('display.max_columns', None)
 trainData = pd.read_csv('train_original.csv')
 print(trainData.head())
 
+print('Total number of question pairs for training: {}'.format(len(trainData)))
 
+qids = pd.Series(trainData['qid1'].tolist() + trainData['qid2'].tolist())
+plt.figure(figsize=(12, 5))
+plt.hist(qids.value_counts(), bins=50)
+plt.yscale('log', nonposy='clip')
+plt.title('Log-Histogram of question appearance counts')
+plt.xlabel('Number of occurences of question')
+plt.ylabel('Number of questions')
+pylab.show()
+print()
